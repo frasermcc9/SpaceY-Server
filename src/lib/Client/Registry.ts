865f419ef839da063a70d70a24b7e701020194fb
Fraser McCallum <fraser@themccallums.net.nz>
2020-06-18 17:42:21 +1200

    Name resolution methods from string

--- src/lib/Client/Registry.ts
+++ src/lib/Client/Registry.ts
@@ -1,11 +1,19 @@
 import { Ship } from "../GameTypes/GameAsset/Buildable/Ship/Ship.ts";
 import { Attachment } from "../GameTypes/GameAsset/Buildable/Attachment/Attachment.ts";
 import { Faction } from "../GameTypes/GameAsset/Faction/Faction.ts";
-import { BaseMaterial } from "../GameTypes/GameAsset/Buildable/Material/BaseMaterial.ts";
+import { Material } from "../GameTypes/GameAsset/Buildable/Material/Material.ts";
 import { Collection } from "../Extensions/Collection.ts";
+import { GameAsset } from "../GameTypes/GameAsset/GameAsset.ts";
 
 export class Registry {
-	public constructor() {}
+	public constructor(copyReg?: Registry) {
+		if (copyReg) {
+			this.shipRegistry = new Collection(copyReg.shipRegistry);
+			this.attachmentRegistry = new Collection(copyReg.attachmentRegistry);
+			this.factionRegistry = new Collection(copyReg.factionRegistry);
+			this.materialRegistry = new Collection(copyReg.materialRegistry);
+		}
+	}
 
 	private readonly shipRegistry = new Collection<string, Ship>();
 	public get ShipRegistry() {
@@ -19,7 +27,7 @@ export class Registry {
 	public get FactionRegistry() {
 		return this.factionRegistry;
 	}
-	private readonly materialRegistry = new Collection<string, BaseMaterial>();
+	private readonly materialRegistry = new Collection<string, Material>();
 	public get MaterialRegistry() {
 		return this.materialRegistry;
 	}
@@ -48,6 +56,35 @@ export class Registry {
 		});
 		return this;
 	}
+
+	public ResolveShipFromName(name: string): Ship | undefined {
+		const result = this.NameResolver<Ship>(name, this.ShipRegistry);
+		if (!result) return undefined;
+		return result;
+	}
+	public ResolveAttachmentFromName(name: string): Attachment | undefined {
+		const result = this.NameResolver<Attachment>(name, this.AttachmentRegistry);
+		if (!result) return undefined;
+		return result;
+	}
+	public ResolveMaterialFromName(name: string): Material | undefined {
+		const result = this.NameResolver<Material>(name, this.MaterialRegistry);
+		if (!result) return undefined;
+		return result;
+	}
+	public ResolveFactionFromName(name: string): Faction | undefined {
+		const result = this.NameResolver<Faction>(name, this.FactionRegistry);
+		if (!result) return undefined;
+		return result;
+	}
+	/**
+	 * @param T the type of object that should be resolved
+	 * @param name the string name of the object
+	 * @param registry the registry to search
+	 */
+	public NameResolver<T>(name: string, registry: Collection<string, T>): T | undefined {
+		return registry.get(name);
+	}
 }
 
 interface IShips {
@@ -60,5 +97,5 @@ interface IFactions {
 	factions: Faction[];
 }
 interface IMaterials {
-	materials: BaseMaterial[];
+	materials: Material[];
 }

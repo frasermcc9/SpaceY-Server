40bab029ee33094b5a94aa07793d1d6c6e838a96
Fraser McCallum <fraser@themccallums.net.nz>
2020-06-18 17:42:07 +1200

    Added method for destroying the client (for unit tests)

--- src/lib/Client/Client.ts
+++ src/lib/Client/Client.ts
@@ -1,23 +1,30 @@
 import { Registry } from "./Registry.ts";
 
 export class Client {
-	//Client Singleton Generation
+	//Section: Client Singleton
 	public static Client: Client;
-	public static GetClient() {
+	public static Get() {
 		if (this.Client == null) {
 			throw new Error("You must explicity create the client. Did you forget to go Client.CreateClient(IClientSettings)?");
 		}
 		return this.Client;
 	}
-	public static CreateClient(ClientSettings: IClientSettings) {
+	public static Create(ClientSettings: IClientSettings) {
+		if (this.Client) throw new Error("A client has already been made. Please use Client.Get() to access it.");
 		this.Client = new Client(ClientSettings);
+	}
+	public static Destroy() {
+		delete this.Client;
 	}
 	private constructor(ClientSettings: IClientSettings) {}
 
-	//Client Instance
+	//Section: Registry
 	private registry: Registry = new Registry();
 	public get Registry(): Registry {
 		return this.registry;
+	}
+	public CopyRegistry(): Registry {
+		return new Registry(this.registry);
 	}
 }
 

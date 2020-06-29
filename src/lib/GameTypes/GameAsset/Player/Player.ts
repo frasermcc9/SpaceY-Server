c59866d92303c4715ae219f942e2a1aab7261ed8
Fraser McCallum <fraser@themccallums.net.nz>
2020-06-30 04:01:44 +1200

    Added support for some inventory manipulation methods

--- src/lib/GameTypes/GameAsset/Player/Player.ts
+++ src/lib/GameTypes/GameAsset/Player/Player.ts
@@ -1,17 +1,89 @@
-import { Ship } from "../Buildable/Ship/Ship.ts"
-import { ReputationCollection } from "../../GameCollections/ReputationCollection.ts";
-import { PlayerInventory } from "./PlayerInventory.ts";
-import { Skin } from "./Skin.ts";
+import { IPlayerDocument, PlayerModel } from "../../../GameApi/Database/Player/PlayerModel";
+import { Ship } from "../Buildable/Ship/Ship";
+import { InventoryBuilder, PlayerInventory } from "./PlayerInventory";
+import { Skin } from "./Skin";
 
+/**
+ * @deprecated this class is not used. IPlayerDocument is the player class.
+ */
 export class Player {
-    private id!: string
+	private uId!: string;
+	public get UId(): string {
+		return this.uId;
+	}
 
-    private ship!: Ship
-    private skin!: Skin
+	private ship!: Ship;
+	private skin!: Skin;
 
-    private reputation!: ReputationCollection
-    private inventory!: PlayerInventory
-    //private location:
+	private inventory!: PlayerInventory;
+	public get Inventory(): PlayerInventory {
+		return this.inventory;
+	}
+	//private location:
 
+	//SECTION - Credits
 
+	public async AddCredits({ amount, noImplicitSave }: { amount: number; noImplicitSave?: boolean }): Promise<boolean> {
+		if (amount < 0) throw new Error("Only positive values can be passed to the incrementCredits method. Consider using decrement to remove credits.");
+		const success: boolean = this.inventory.AddCredits({ amount: amount });
+		if (success && !noImplicitSave) {
+			await this.save();
+		}
+		return success;
+	}
+	public async RemoveCredits({ amount, noImplicitSave }: { amount: number; noImplicitSave?: boolean }): Promise<boolean> {
+		if (amount < 0) throw new Error("Only positive values can be passed to the decrementCredits method. Consider using increment to add credits.");
+		const success: boolean = this.inventory.AddCredits({ amount: -amount });
+		if (success && !noImplicitSave) {
+			await this.save();
+		}
+		return success;
+	}
+	public get Credits(): number {
+		return this.inventory.Credits;
+	}
+
+	//SECTION - Materials
+
+	/**
+	 * Increases the given item by the amount.
+	 * @param itemName
+	 * @param quantity
+	 * @returns codes: 1-Success, 2-Item Not Found
+	 */
+	public async MaterialIncrement(name: string, quantity: number): Promise<{ success: boolean; amount: number; code: 1 | 2; error?: string }> {
+		const result = this.inventory.Materials.Increase(name, quantity);
+		await this.save();
+		return result;
+	}
+
+	/**
+	 * Reduces a given item by the given amount. Cannot reduce below zero.
+	 * @param itemName
+	 * @param quantity
+	 * @returns codes: 1-Success, 2-Item Not Found, 3-Not Enough Resources
+	 */
+	public async MaterialDecrement(name: string, quantity: number): Promise<{ success: boolean; amount: number; code: 1 | 2 | 3; error?: string }> {
+		const result = this.inventory.Materials.ReduceToNonNegative(name, quantity);
+		await this.save();
+		return result;
+	}
+
+	public async save(): Promise<void> {
+		await PlayerModel.updateOne({ uId: this.uId }, { uId: this.uId, inventory: this.inventory.GetGeneric(), ship: this.ship, skin: this.skin });
+	}
+
+	public constructor(data: IPlayerDocument) {
+		this.uId = data.uId;
+		this.ship = data.ship;
+		this.skin = data.skin;
+		this.inventory = new InventoryBuilder()
+			.SetCredits(data.inventory.credits)
+			.SetReputation({ data: data.inventory.reputation })
+			.SetAttachments({ data: data.inventory.attachments })
+			.SetMaterials({ data: data.inventory.materials })
+			.SetShips({ data: data.inventory.ships })
+			.SetTokens(data.inventory.tokens)
+			.Build();
+	}
 }
--- src/lib/GameTypes/GameAsset/Player/PlayerInventory.ts
+++ src/lib/GameTypes/GameAsset/Player/PlayerInventory.ts
@@ -1,25 +1,80 @@
-import { MaterialCollection, IMaterialCollectionOptions } from "../../GameCollections/MaterialCollection.ts";
-import { ShipCollection, IShipCollectionOptions } from "../../GameCollections/ShipCollection.ts";
-import { AttachmentCollection, IAttachmentCollectionOptions } from "../../GameCollections/AttachmentCollection.ts";
-import { Client } from "../../../Client/Client.ts";
+import { MaterialCollection, IMaterialCollectionOptions } from "../../GameCollections/MaterialCollection";
+import { ShipCollection, IShipCollectionOptions } from "../../GameCollections/ShipCollection";
+import { AttachmentCollection, IAttachmentCollectionOptions } from "../../GameCollections/AttachmentCollection";
+import { Client } from "../../../Client/Client";
+import { ReputationCollection, IReputationCollectionOptions } from "../../GameCollections/ReputationCollection";
+import { IPlayerDocument } from "../../../GameApi/Database/Player/PlayerModel";
+import { Material } from "../Material/Material";
 
 /**
  * Represents an inventory of a player. Please @see InventoryBuilder too create an inventory.
  */
 export class PlayerInventory {
 	private materials: MaterialCollection;
+	public get Materials(): MaterialCollection {
+		return this.materials;
+	}
 	private ships: ShipCollection;
 	private attachments: AttachmentCollection;
-
+	private reputation: ReputationCollection;
 	private credits: number;
 	private tokens: number;
 
-	constructor(options: IPlayerInventoryOptions) {
-		this.materials = options.materialOptions || new MaterialCollection();
-		this.ships = options.shipOptions || new ShipCollection();
-		this.attachments = options.attachmentOptions || new AttachmentCollection();
-		this.credits = options.credits || Client.Get().NewUserCreditValue;
-		this.tokens = options.tokens || 0;
+	public get Credits(): number {
+		return this.credits;
+	}
+	public get Ships(): ShipCollection {
+		return this.ships;
+	}
+	public get Attachments(): AttachmentCollection {
+		return this.attachments;
+	}
+	public get Reputation(): ReputationCollection {
+		return this.reputation;
+	}
+	public get Tokens(): number {
+		return this.tokens;
+	}
+
+	public constructor(options: IPlayerInventoryOptions) {
+		if (isInventory(options)) {
+			this.materials = options.materialOptions || new MaterialCollection();
+			this.ships = options.shipOptions || new ShipCollection();
+			this.attachments = options.attachmentOptions || new AttachmentCollection();
+			this.reputation = options.reputationOptions || new ReputationCollection();
+			this.credits = options.credits || Client.Get().Registry.DefaultCredits;
+			this.tokens = options.tokens || 0;
+		} else {
+			throw new Error("Invalid inventory creation.");
+		}
+	}
+
+	public AddCredits({ amount }: { amount: number }): boolean {
+		let checkAmount = this.credits;
+		checkAmount += amount;
+		if (checkAmount >= 0) {
+			this.credits = checkAmount;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public GetGeneric(): {
+		credits: number;
+		tokens: number;
+		materials: Map<string, number>;
+		ships: Map<string, number>;
+		attachments: Map<string, number>;
+		reputation: Map<string, number>;
+	} {
+		return {
+			attachments: this.attachments,
+			credits: this.credits,
+			materials: this.materials,
+			reputation: this.reputation,
+			ships: this.ships,
+			tokens: this.tokens,
+		};
 	}
 }
 
@@ -27,6 +82,7 @@ export class InventoryBuilder {
 	private materials?: IMaterialCollectionOptions;
 	private ships?: IShipCollectionOptions;
 	private attachments?: IAttachmentCollectionOptions;
+	private reputation?: IReputationCollectionOptions;
 
 	private credits?: number;
 	private tokens?: number;
@@ -43,6 +99,10 @@ export class InventoryBuilder {
 		this.attachments = attachmentOptions;
 		return this;
 	}
+	public SetReputation(attachmentOptions: IReputationCollectionOptions): InventoryBuilder {
+		this.reputation = attachmentOptions;
+		return this;
+	}
 	public SetCredits(credits: number): InventoryBuilder {
 		this.credits = credits;
 		return this;
@@ -57,9 +117,21 @@ export class InventoryBuilder {
 			attachmentOptions: new AttachmentCollection({ data: this.attachments?.data }),
 			materialOptions: new MaterialCollection({ data: this.materials?.data }),
 			shipOptions: new ShipCollection({ data: this.ships?.data }),
+			reputationOptions: new ReputationCollection({ data: this.reputation?.data }),
 			credits: this.credits,
 			tokens: this.tokens,
 		});
+	}
+
+	public GenericBuild(): { credits: number; tokens: number; materials: {}; ships: {}; attachments: {}; reputation: {} } {
+		return {
+			attachments: new AttachmentCollection(),
+			credits: Client.Get().Registry.DefaultCredits,
+			materials: new MaterialCollection(),
+			reputation: new ReputationCollection(),
+			ships: new ShipCollection(),
+			tokens: 0,
+		};
 	}
 }
 
@@ -67,7 +139,15 @@ interface IPlayerInventoryOptions {
 	materialOptions?: MaterialCollection;
 	shipOptions?: ShipCollection;
 	attachmentOptions?: AttachmentCollection;
+	reputationOptions?: ReputationCollection;
 
 	credits?: number;
 	tokens?: number;
+}
+
+function isDocument(object: any): object is IPlayerDocument {
+	return "inventory" in object;
+}
+function isInventory(object: any): object is IPlayerInventoryOptions {
+	return "credits" in object;
 }

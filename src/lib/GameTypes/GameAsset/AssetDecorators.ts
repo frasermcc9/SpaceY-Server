f4546ae916d693f3cdfdba707a37f9aebdf88bb6
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-03 02:49:44 +1200

    Implemented asset decorator methods

--- src/lib/GameTypes/GameAsset/AssetDecorators.ts
+++ src/lib/GameTypes/GameAsset/AssetDecorators.ts
@@ -1,4 +1,6 @@
-import { GameAsset, Sellable, ISellInfo, IGameAsset, IBlueprintInfo } from "./GameAsset";
+import { GameAsset, Sellable, ISellInfo, IGameAsset, IBlueprintInfo, Buildable } from "./GameAsset";
+import { Client } from "../../Client/Client";
+import { Player } from "./Player/Player";
 
 export abstract class GameAssetDecorator implements IGameAsset {
 	protected asset: GameAsset;
@@ -16,8 +18,12 @@ export abstract class GameAssetDecorator implements IGameAsset {
 }
 
 export class SellableDecorator extends GameAssetDecorator implements Sellable {
-	public constructor(item: GameAsset) {
-		super(item);
+	public constructor(item: string | GameAsset) {
+		if (typeof item == "string") {
+			const obj = Client.Reg.AnyResolve(item);
+			if (obj == undefined) throw new TypeError(`Item named ${item} instantiated as SellableDecorator when item does not exist in registry.`);
+			super(obj);
+		} else super(item);
 	}
 
 	public get PriceData(): ISellInfo {
@@ -25,12 +31,25 @@ export class SellableDecorator extends GameAssetDecorator implements Sellable {
 	}
 }
 
-export class BuildableDecorator extends GameAssetDecorator implements Sellable {
+export class BuildableDecorator extends GameAssetDecorator implements Buildable {
 	public constructor(item: GameAsset) {
 		super(item);
 	}
 
-	public get PriceData(): IBlueprintInfo {
+	public get Blueprint(): IBlueprintInfo {
 		return this.asset.Blueprint;
+	}
+	/**
+	 * @param player
+	 * @returns codes: 200-Success, 403-Insufficient resources, 405-Item not buildable, 500-Internal Error
+	 */
+	public async Build(player: Player): Promise<{ code: number; failures: string[] }> {
+		const bp = this.Blueprint.blueprint;
+		if (bp == undefined) return { code: 405, failures: [] };
+		const result = await player.InventorySubtract("materials", bp);
+		if (result.code != 200) return result;
+		const editResult = await player.AutoInventoryEdit(this.asset.Name, 1);
+		if (editResult.success) return result;
+		else return { code: 500, failures: [] };
 	}
 }

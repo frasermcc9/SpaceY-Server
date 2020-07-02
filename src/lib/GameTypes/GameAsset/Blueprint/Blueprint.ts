e886a5d8230f6cc4afda5e5d47b519ce8ea4218b
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-03 02:50:17 +1200

    Implemented class

--- src/lib/GameTypes/GameAsset/Blueprint/Blueprint.ts
+++ src/lib/GameTypes/GameAsset/Blueprint/Blueprint.ts
@@ -1,3 +1,17 @@
-export class Blueprint {}
+import { IGenerationOptions } from "../../GameCollections/GameCollectionBase";
+import { MaterialCollection } from "../../GameCollections/MaterialCollection";
 
+export class Blueprint extends MaterialCollection {
+	public constructor(materials: Map<string, number>) {
+		super({ data: materials });
+	}
+}
 
+export class BlueprintBuilder {
+	public ManualBuild(data: Map<string, number>): Blueprint {
+		return new Blueprint(data);
+	}
+	public AutoBuild(options: IGenerationOptions) {
+		return new Blueprint(new MaterialCollection().GenerateCollection(options));
+	}
+}

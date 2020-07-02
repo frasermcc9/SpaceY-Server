97abc62af4c6abb25e54e886a822767df65a25b3
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-03 02:54:09 +1200

    changed const exports to functions to fix bug where client would get called before creation

--- src/test/TestUtil.ts
+++ src/test/TestUtil.ts
@@ -1,25 +1,25 @@
 import { Client } from "../lib/Client/Client";
 import { Material, MaterialBuilder } from "../lib/GameTypes/GameAsset/Material/Material";
-import { Blueprint } from "../lib/GameTypes/GameAsset/Blueprint/Blueprint";
+import { Blueprint, BlueprintBuilder } from "../lib/GameTypes/GameAsset/Blueprint/Blueprint";
 import { Ship } from "../lib/GameTypes/GameAsset/Buildable/Ship/Ship";
 import { Faction } from "../lib/GameTypes/GameAsset/Faction/Faction";
 import { Attachment } from "../lib/GameTypes/GameAsset/Buildable/Attachment/Attachment";
 
 export function GenerateMaterialsForActiveClient() {
 	const client = Client.Get();
-	client.Registry.RegisterMaterials({ materials: GENERATED_MATERIALS });
+	client.Registry.RegisterMaterials({ materials: GENERATED_MATERIALS() });
 }
 export function GenerateShipsForActiveClient() {
 	const client = Client.Get();
-	client.Registry.RegisterShips({ ships: GENERATED_SHIPS });
+	client.Registry.RegisterShips({ ships: GENERATED_SHIPS() });
 }
 export function GenerateFactionsForActiveClient() {
 	const client = Client.Get();
-	client.Registry.RegisterFactions({ factions: GENERATED_FACTIONS });
+	client.Registry.RegisterFactions({ factions: GENERATED_FACTIONS() });
 }
 export function GenerateAttachmentsForActiveClient() {
 	const client = Client.Get();
-	client.Registry.RegisterAttachments({ attachments: GENERATED_ATTACHMENTS });
+	client.Registry.RegisterAttachments({ attachments: GENERATED_ATTACHMENTS() });
 }
 
 export function GenerateClientSet() {
@@ -31,24 +31,37 @@ export function GenerateClientSet() {
 /**
  * Iron, Gold, Food, Tech
  */
-export const GENERATED_MATERIALS = [
-	new MaterialBuilder({ name: "Iron", description: "A small iron nugget." }).EnableSell(25).EnableMine().SetRarity(4).Build(),
-	new MaterialBuilder({ name: "Gold", description: "A small gold nugget." }).EnableSell(75).EnableMine().SetRarity(10).Build(),
-	new MaterialBuilder({ name: "Food", description: "Food for one person." }).EnableSell(5).SetRarity(1).Build(),
-	new MaterialBuilder({ name: "Tech", description: "Pile of tech pieces." }).EnableSell(50).SetRarity(6).Build(),
-];
-export const GENERATED_SHIPS = [
-	new Ship({ description: "A small but agile ship", name: "Shuttle" }),
-	new Ship({ description: "A medium sized vehicle", name: "Warship" }),
-	new Ship({ description: "A flagship destroyer", name: "Destroyer" }),
-];
-export const GENERATED_FACTIONS = [
-	new Faction({ description: "Like you. And me.", name: "Humans" }),
-	new Faction({ description: "The great alliance of the galaxy", name: "Alliance" }),
-	new Faction({ description: "The sentient robotic race", name: "Cyborgs" }),
-];
-export const GENERATED_ATTACHMENTS = [
-	new Attachment({ name: "Blaster", description: "Standard Issue Blaster" }),
-	new Attachment({ name: "Space Sword", description: "Not very practical" }),
-	new Attachment({ name: "Obliterator", description: "Quite scary, really" }),
-];
+export function GENERATED_MATERIALS() {
+	return [
+		new MaterialBuilder({ name: "Iron", description: "A small iron nugget." }).EnableSell(25).EnableMine().SetRarity(4).Build(),
+		new MaterialBuilder({ name: "Gold", description: "A small gold nugget." }).EnableSell(75).EnableMine().SetRarity(10).Build(),
+		new MaterialBuilder({ name: "Food", description: "Food for one person." }).EnableSell(5).SetRarity(1).Build(),
+		new MaterialBuilder({ name: "Tech", description: "Pile of tech pieces." }).EnableSell(50).SetRarity(6).Build(),
+	];
+}
+export function GENERATED_SHIPS() {
+	return [
+		new Ship({
+			description: "A small but agile ship",
+			name: "Shuttle",
+			blueprint: new BlueprintBuilder().AutoBuild({ value: 1000, rarity: true, minRarity: 0, maxRarity: 10, centralRarity: 3 }),
+			cost: 1500,
+		}),
+		new Ship({ description: "A medium sized vehicle", name: "Warship" }),
+		new Ship({ description: "A flagship destroyer", name: "Destroyer" }),
+	];
+}
+export function GENERATED_FACTIONS() {
+	return [
+		new Faction({ description: "Like you. And me.", name: "Humans" }),
+		new Faction({ description: "The great alliance of the galaxy", name: "Alliance" }),
+		new Faction({ description: "The sentient robotic race", name: "Cyborgs" }),
+	];
+}
+export function GENERATED_ATTACHMENTS() {
+	return [
+		new Attachment({ name: "Blaster", description: "Standard Issue Blaster" }),
+		new Attachment({ name: "Space Sword", description: "Not very practical" }),
+		new Attachment({ name: "Obliterator", description: "Quite scary, really" }),
+	];
+}

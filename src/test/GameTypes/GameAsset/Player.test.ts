bfc6678feb4c7ef6e2621d17e26692dccf9ef3b1
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-04 03:38:53 +1200

    updated imports

--- src/test/GameTypes/GameAsset/Player.test.ts
+++ src/test/GameTypes/GameAsset/Player.test.ts
@@ -3,13 +3,13 @@ import { GenerateClientSet, GENERATED_SHIPS } from "../../TestUtil";
 import { PlayerModel } from "../../../lib/GameApi/Database/Player/PlayerModel";
 import { AsteroidBuilder } from "../../../lib/GameTypes/GameMechanics/Asteroid";
 import { default as must } from "must";
-import { Ship } from "../../../lib/GameTypes/GameAsset/Buildable/Ship/Ship";
+import { Ship } from "../../../lib/GameTypes/GameAsset/Ship/Ship";
 import { BlueprintBuilder } from "../../../lib/GameTypes/GameAsset/Blueprint/Blueprint";
 import { BuildableDecorator } from "../../../lib/GameTypes/GameAsset/AssetDecorators";
 require("must/register");
 
 describe("Player Tests", async () => {
-	describe.only("Blueprint and Build Tests", async () => {
+	describe("Player Building from Blueprint Tests", async () => {
 		it("Should reject since the player cannot afford the materials.", async () => {
 			const ship = GENERATED_SHIPS()[0];
 			const Player = await PlayerModel.findOneOrCreate({ uId: "1" });
@@ -21,6 +21,6 @@ describe("Player Tests", async () => {
 			const MaterialsToAdd = new Map<string, number>().set("Iron", 1000).set("Gold", 1000).set("Tech", 1000).set("Food", 1000);
 			Player.InventorySum("materials", MaterialsToAdd);
 			(await new BuildableDecorator(ship).Build(Player)).code.must.eql(200);
-		});
+        });
 	});
 });

f909f6ec9c6c2b35a7c8d991c44d0f81df560694
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-04 03:36:48 +1200

    Started faction implementation

--- src/lib/GameTypes/GameAsset/Faction/Faction.ts
+++ src/lib/GameTypes/GameAsset/Faction/Faction.ts
@@ -1,3 +1,30 @@
 import { GameAsset } from "../GameAsset";
 
-export class Faction extends GameAsset {}
+export class Faction extends GameAsset {
+	private techLevel: number;
+
+	public constructor(options: FactionOptions) {
+		super({ name: options.description, description: options.description });
+		this.techLevel = options.techLevel ?? 0;
+	}
+}
+
+export class FactionBuilder {
+	private name: string;
+	private description: string;
+	private techLevel?: number;
+	public constructor(name: string, description: string, techLevel?: number) {
+		this.name = name;
+		this.description = description;
+		this.techLevel = techLevel;
+	}
+	public Build(): Faction {
+		return new Faction({ name: this.name, description: this.description, techLevel: this.techLevel });
+	}
+}
+
+interface FactionOptions {
+	name: string;
+	description: string;
+	techLevel?: number;
+}

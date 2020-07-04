3575819dfe84df4e068feb4d7c62695ceedf2ac4
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-05 02:55:46 +1200

    Extension to materialcollection that limits the number of items based on ship cargo

---
+++ src/lib/GameTypes/GameCollections/PlayerMaterialCollection.ts
@@ -0,0 +1,28 @@
+import { MaterialCollection, IMaterialCollectionOptions } from "./MaterialCollection";
+import { Player } from "../GameAsset/Player/Player";
+import { Client } from "../../main";
+
+export class PlayerMaterialCollection extends MaterialCollection {
+	private owner?: Player;
+
+	public maxCollectionSize(): number {
+		if (this.owner == undefined) return Infinity;
+		return this.owner.getShipWrapper().ShipStatistics.totalCargo;
+	}
+
+	/**@override */
+	public set(key: string, value: number): this {
+		const current = this.get(key);
+		const diff = this.maxCollectionSize() - (this.CollectionSize - (current ?? 0) + value);
+		if (diff >= 0) return super.set(key, value);
+		else return super.set(key, value + diff);
+	}
+
+	public constructor(options?: IMaterialCollectionOptions) {
+		super(options);
+	}
+
+	public set Owner(player: Player) {
+		this.owner = player;
+	}
+}

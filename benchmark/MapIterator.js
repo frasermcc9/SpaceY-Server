ba0f8a6a41111001a82860d06564108f6fa66eb6
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-05 00:44:59 +1200

    Testing various methods

---
+++ benchmark/MapIterator.js
@@ -0,0 +1,33 @@
+const { performance } = require("perf_hooks");
+
+const n = 10; //array size
+
+const Base = new Map();
+for (let i = 0; i < n; i++) {
+	Base.set(i, Math.random() * 1000);
+}
+
+DirectForEach();
+Spread();
+
+function DirectForEach() {
+	const copy = new Map(Base);
+    const TimerStart = performance.now();
+    
+    copy.forEach((_, key) => copy.set(key, 0));
+    
+	const TimerEnd = performance.now();
+	console.log(`ForLoop method: ${TimerEnd - TimerStart}`);
+}
+
+function Spread() {
+	const copy = new Map(Base);
+    const TimerStart = performance.now();
+    
+    [...copy.keys()].forEach((key) => {
+        copy.set(key, false);
+      });
+    
+	const TimerEnd = performance.now();
+	console.log(`Spread method: ${TimerEnd - TimerStart}`);
+}

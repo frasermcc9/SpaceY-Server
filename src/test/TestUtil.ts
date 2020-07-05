8f2b9f640d578727863e8314840e3c40632aa4fb
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-05 14:43:33 +1200

    Added more object construction details

--- src/test/TestUtil.ts
+++ src/test/TestUtil.ts
@@ -3,8 +3,15 @@ import { Material, MaterialBuilder } from "../lib/GameTypes/GameAsset/Material/M
 import { Blueprint, BlueprintBuilder } from "../lib/GameTypes/GameAsset/Blueprint/Blueprint";
 import { Ship, ShipBuilder } from "../lib/GameTypes/GameAsset/Ship/Ship";
 import { Faction, FactionBuilder } from "../lib/GameTypes/GameAsset/Faction/Faction";
-import { Attachment, AttachmentBuilder, AttachmentType, AttachmentReport } from "../lib/GameTypes/GameAsset/Attachment/Attachment";
+import {
+	Attachment,
+	AttachmentBuilder,
+	AttachmentType,
+	AttachmentReport,
+	MineEvent,
+} from "../lib/GameTypes/GameAsset/Attachment/Attachment";
 import { ShipWrapper } from "../lib/GameTypes/GameAsset/Ship/ShipWrapper";
+import { Asteroid } from "../lib/GameTypes/GameMechanics/Asteroid";
 
 export function GenerateMaterialsForActiveClient() {
 	const client = Client.Get();
@@ -34,8 +41,16 @@ export function GenerateClientSet() {
  */
 export function GENERATED_MATERIALS() {
 	return [
-		new MaterialBuilder({ name: "Iron", description: "A small iron nugget." }).EnableSell(25).EnableMine().SetRarity(4).Build(),
-		new MaterialBuilder({ name: "Gold", description: "A small gold nugget." }).EnableSell(75).EnableMine().SetRarity(10).Build(),
+		new MaterialBuilder({ name: "Iron", description: "A small iron nugget." })
+			.EnableSell(25)
+			.EnableMine()
+			.SetRarity(4)
+			.Build(),
+		new MaterialBuilder({ name: "Gold", description: "A small gold nugget." })
+			.EnableSell(75)
+			.EnableMine()
+			.SetRarity(10)
+			.Build(),
 		new MaterialBuilder({ name: "Food", description: "Food for one person." }).EnableSell(5).SetRarity(1).Build(),
 		new MaterialBuilder({ name: "Tech", description: "Pile of tech pieces." }).EnableSell(50).SetRarity(6).Build(),
 	];
@@ -44,19 +59,44 @@ export function GENERATED_SHIPS() {
 	return [
 		new ShipBuilder({ name: "Shuttle", description: "A small but agile ship" })
 			.EnableSell(1500)
-			.EnableBuildable(new BlueprintBuilder().AutoBuild({ value: 1000, rarity: true, minRarity: 0, maxRarity: 10, centralRarity: 3 }))
+			.EnableBuildable(
+				new BlueprintBuilder().AutoBuild({
+					value: 1000,
+					rarity: true,
+					minRarity: 0,
+					maxRarity: 10,
+					centralRarity: 3,
+				})
+			)
 			.SetStats({ baseHp: 100, baseShield: 50 })
 			.SetWeapons({ heavyCap: 3, primaryCap: 3, generalCap: 2 })
 			.Build(),
 		new ShipBuilder({ name: "Warship", description: "A medium sized vehicle" })
 			.EnableSell(3000)
-			.EnableBuildable(new BlueprintBuilder().AutoBuild({ value: 2550, rarity: true, minRarity: 0, maxRarity: 10, centralRarity: 5 }))
+			.EnableBuildable(
+				new BlueprintBuilder().AutoBuild({
+					value: 2550,
+					rarity: true,
+					minRarity: 0,
+					maxRarity: 10,
+					centralRarity: 5,
+				})
+			)
 			.SetStats({ baseHp: 150, baseShield: 90, baseCargo: 750 })
 			.Build(),
 		new ShipBuilder({ name: "Destroyer", description: "A flagship destroyer" })
 			.EnableSell(4500)
-			.EnableBuildable(new BlueprintBuilder().AutoBuild({ value: 4000, rarity: true, minRarity: 0, maxRarity: 10, centralRarity: 7 }))
+			.EnableBuildable(
+				new BlueprintBuilder().AutoBuild({
+					value: 4000,
+					rarity: true,
+					minRarity: 0,
+					maxRarity: 10,
+					centralRarity: 7,
+				})
+			)
 			.SetStats({ baseHp: 200, baseShield: 170, baseCargo: 8000 })
+			.SetWeapons({ generalCap: 5, heavyCap: 5, minerCap: 1, primaryCap: 5, shieldCap: 5 })
 			.Build(),
 	];
 }
@@ -76,13 +116,47 @@ export function GENERATED_ATTACHMENTS() {
 		friendly.decrementStatistics({ hp: 20 });
 		return { message: `New Health: ${friendly.ShipStatistics.totalHp}` };
 	};
+	const LaserMine: (inputCollection: Asteroid) => AttachmentReport = (asteroid) => {
+		asteroid.forEach((val, key) => {
+			asteroid.set(key, val * 2);
+		});
+		return { message: "Success" };
+	};
 	return [
-		new AttachmentBuilder({ name: "Blaster", description: "Standard issue blaster", type: AttachmentType.PRIMARY, techLevel: 2 }).Build(),
-		new AttachmentBuilder({ name: "Space Sword", description: "Not very practical", type: AttachmentType.HEAVY, techLevel: 1 }).Build(),
-		new AttachmentBuilder({ name: "Obliterator", description: "This one will hurt", type: AttachmentType.HEAVY, techLevel: 6 }).Build(),
-		new AttachmentBuilder({ name: "Iron Plating", description: "Tough metal", techLevel: 2, type: AttachmentType.GENERAL })
+		new AttachmentBuilder({
+			name: "Blaster",
+			description: "Standard issue blaster",
+			type: AttachmentType.PRIMARY,
+			techLevel: 2,
+		}).Build(),
+		new AttachmentBuilder({
+			name: "Space Sword",
+			description: "Not very practical",
+			type: AttachmentType.HEAVY,
+			techLevel: 1,
+		}).Build(),
+		new AttachmentBuilder({
+			name: "Obliterator",
+			description: "This one will hurt",
+			type: AttachmentType.HEAVY,
+			techLevel: 6,
+		}).Build(),
+		new AttachmentBuilder({
+			name: "Iron Plating",
+			description: "Tough metal",
+			techLevel: 2,
+			type: AttachmentType.GENERAL,
+		})
 			.EquipFn(PlatingEquip)
 			.UnequipFn(PlatingUnequip)
+			.Build(),
+		new AttachmentBuilder({
+			name: "Perfected Mining Laser",
+			description: "The pinnacle of mining technology",
+			type: AttachmentType.MINER,
+			techLevel: 8,
+		})
+			.MineFn(LaserMine)
 			.Build(),
 	];
 }

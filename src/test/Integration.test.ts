10e7f588a7360edc94b4f6e80bd40296a4a8c15f
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-07 20:34:15 +1200

    Added default location

--- src/test/Integration.test.ts
+++ src/test/Integration.test.ts
@@ -4,6 +4,9 @@ import { PlayerModel } from "../lib/GameApi/Database/Player/PlayerModel";
 import { GenerateClientSet, generateIntegrationSet } from "./TestUtil";
 import { throws } from "assert";
 import { default as must } from "must";
+import { SpacemapNodeBuilder, WarpPower } from "../lib/GameTypes/GameSpacemap/SpacemapNode";
+import { Faction, FactionBuilder } from "../lib/GameTypes/GameAsset/Faction/Faction";
+import { Spacemap } from "../lib/GameTypes/GameSpacemap/Spacemap";
 require("must/register");
 
 const DEFAULT_CREDITS = 10000;
@@ -19,6 +22,15 @@ before(async () => {
 	});
 	GenerateClientSet();
 	connect();
+	const SM = new Spacemap();
+	const SN = new SpacemapNodeBuilder({
+		name: "Default",
+		requiredWarp: WarpPower.NONE,
+		faction: new FactionBuilder({ name: "G", techLevel: 2, description: "Test" }).Build(),
+	}).build();
+	SM.addNode(SN);
+	Client.Reg.registerSpacemap(SM);
+	Client.Reg.DefaultLocation = SN;
 });
 
 beforeEach(async () => {

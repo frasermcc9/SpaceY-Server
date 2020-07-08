daf071f70bc5393a406319bd1561a7947c7fc6cd
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-09 00:28:05 +1200

    Added more tests

--- src/test/GameTypes/GameAsset/Player.test.ts
+++ src/test/GameTypes/GameAsset/Player.test.ts
@@ -12,15 +12,19 @@ describe("Player Tests", async () => {
 	describe("Player Building from Blueprint Tests", async () => {
 		it("Should reject since the player cannot afford the materials.", async () => {
 			const ship = GENERATED_SHIPS()[0];
-            const Player = await PlayerModel.findOneOrCreate({ uId: "1" });
-            await Player.discoverBlueprint(ship);
+			const Player = await PlayerModel.findOneOrCreate({ uId: "1" });
+			await Player.discoverBlueprint(ship);
 			(await new BuildableDecorator(ship).Build(Player)).code.must.eql(403.1);
 		});
 		it("Should accept since the player can afford the materials.", async () => {
 			const ship = GENERATED_SHIPS()[0];
-            const Player = await PlayerModel.findOneOrCreate({ uId: "1" });
-            await Player.discoverBlueprint(ship);
-			const MaterialsToAdd = new Map<string, number>().set("Iron", 1000).set("Gold", 1000).set("Tech", 1000).set("Food", 1000);
+			const Player = await PlayerModel.findOneOrCreate({ uId: "1" });
+			await Player.discoverBlueprint(ship);
+			const MaterialsToAdd = new Map<string, number>()
+				.set("Iron", 1000)
+				.set("Gold", 1000)
+				.set("Tech", 1000)
+				.set("Food", 1000);
 			Player.setShip("Destroyer");
 			Player.InventorySum("materials", MaterialsToAdd);
 			(await new BuildableDecorator(ship).Build(Player)).code.must.eql(200);
@@ -35,6 +39,32 @@ describe("Player Tests", async () => {
 
 			const savedPlayer = await PlayerModel.findOneOrCreate({ uId: "1" });
 			savedPlayer.AutoInventoryRetrieve("Iron").amount.must.eql(cargo);
+		});
+	});
+	describe("Player EXP Tests", async () => {
+		it("Should add the xp", async () => {
+			const Player = await PlayerModel.findOneOrCreate({ uId: "1" });
+			Player.Level.must.eql(1);
+			Player.addExp(84);
+			Player.Exp.must.eql(114);
+			Player.Level.must.eql(3);
+		});
+		it("Should return the correct amount of xp needed to level up", async () => {
+			const Player = await PlayerModel.findOneOrCreate({ uId: "1" });
+			Player.Level.must.eql(1);
+			Player.addExp(83);
+			Player.Exp.must.eql(113);
+			Player.Level.must.eql(2);
+			Player.ExpToNextLevel.must.eql(1);
+		});
+		it("Should emit an event when the player levels up", async () => {
+			let success: boolean = false;
+			Client.EventMan.on("LevelUp", () => {
+				success = true;
+			});
+			const Player = await PlayerModel.findOneOrCreate({ uId: "1" });
+			Player.addExp(1000);
+			success.must.be.true();
 		});
 	});
 });

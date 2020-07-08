20ad2f4c714f11dcad0608de24db76b17956d024
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-08 17:11:54 +1200

    Added event manager class which links with client class, where global events can be listened for

---
+++ src/lib/Client/EventManager.ts
@@ -0,0 +1,7 @@
+import { EventEmitter } from "events";
+
+export class EventManager extends EventEmitter {}
+
+export declare interface EventManager {
+	on(event: "LevelUp", listener: ({ uId, level }: { uId: string; level: number }) => void): this;
+}

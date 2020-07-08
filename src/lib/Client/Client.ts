560ce15d704c9a3c59af5fdc8c63abd8a79a51a6
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-08 17:08:41 +1200

    Added test mode functions

--- src/lib/Client/Client.ts
+++ src/lib/Client/Client.ts
@@ -1,22 +1,18 @@
 import { Registry } from "./Registry";
-import { Ship } from "../GameTypes/GameAsset/Ship/Ship";
 import { connect } from "../GameApi/Database/Database";
+import { EventManager } from "./EventManager";
 
 export class Client {
+	public static TEST = false;
 	//Section: Declared Constants
 	public readonly ConsoleLogging: boolean;
 	//Section: Client Singleton
 	public static Client: Client;
 	public static Get() {
 		if (this.Client == null) {
-			throw new Error(
-				"You must explicity create the client. Did you forget to go Client.CreateClient(IClientSettings)?"
-			);
+			throw new Error("You must explicity create the client. Did you forget to go Client.CreateClient(IClientSettings)?");
 		}
 		return this.Client;
-	}
-	public static get Reg(): Registry {
-		return this.Get().Registry;
 	}
 
 	public static Create(ClientSettings: IClientSettings) {
@@ -29,13 +25,15 @@ export class Client {
 	private constructor(clientSettings: IClientSettings) {
 		this.uri = clientSettings.databaseUri;
 		this.dbName = clientSettings.databaseName;
-		this.ConsoleLogging = clientSettings.consoleLogging || false;
-		this.registry.DefaultCredits = clientSettings.defaultCredits || 0;
+		this.ConsoleLogging = clientSettings.consoleLogging ?? false;
+		this.registry.DefaultCredits = clientSettings.defaultCredits ?? 0;
 		this.registry.MaxRarity = clientSettings.maximumRarity;
 		this.registry.MaxTech = clientSettings.maximumTechLevel;
+
+		Client.TEST = clientSettings.testMode ?? false;
 	}
 
-	//Section: Registry
+	//#region Registry
 	private registry: Registry = new Registry();
 	public get Registry(): Registry {
 		return this.registry;
@@ -44,7 +42,12 @@ export class Client {
 		return new Registry(this.registry);
 	}
 
-	//Section: Database
+	public static get Reg(): Registry {
+		return this.Get().Registry;
+	}
+	//#endregion registry
+
+	//#region Database
 	private uri: string = "";
 	public get Uri() {
 		return this.uri;
@@ -56,6 +59,18 @@ export class Client {
 	public Connect(): void {
 		connect();
 	}
+	//#endregion Database
+
+	//#region EventManager
+
+	private eventManager: EventManager = new EventManager();
+	public static get EventMan(): EventManager {
+		return this.Get().eventManager;
+	}
+	public get EventMan(): EventManager {
+		return this.eventManager;
+	}
+	//#endregion EventManager
 }
 
 interface IClientSettings {
@@ -65,4 +80,5 @@ interface IClientSettings {
 	defaultCredits?: number;
 	maximumRarity: number;
 	maximumTechLevel: number;
+	testMode?: boolean;
 }

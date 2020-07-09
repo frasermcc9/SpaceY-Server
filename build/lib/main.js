9377c4ed45a47c598dbfae29fe4da83efa8df448
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-09 20:42:13 +1200

    Added default assets to exports

--- build/lib/main.js
+++ build/lib/main.js
@@ -11,3 +11,13 @@ Object.defineProperty(exports, "AsteroidBuilder", { enumerable: true, get: funct
 var Database_1 = require("./GameApi/Database/Database");
 Object.defineProperty(exports, "connect", { enumerable: true, get: function () { return Database_1.connect; } });
 Object.defineProperty(exports, "disconnect", { enumerable: true, get: function () { return Database_1.disconnect; } });
+var AttachmentGen_1 = require("../example/AttachmentGen");
+Object.defineProperty(exports, "AttachmentGenerator", { enumerable: true, get: function () { return AttachmentGen_1.AttachmentGenerator; } });
+var FactionGen_1 = require("../example/FactionGen");
+Object.defineProperty(exports, "FactionGenerator", { enumerable: true, get: function () { return FactionGen_1.FactionGenerator; } });
+var MaterialGen_1 = require("../example/MaterialGen");
+Object.defineProperty(exports, "MaterialGenerator", { enumerable: true, get: function () { return MaterialGen_1.MaterialGenerator; } });
+var NodeGen_1 = require("../example/NodeGen");
+Object.defineProperty(exports, "NodeGenerator", { enumerable: true, get: function () { return NodeGen_1.NodeGenerator; } });
+var ShipGen_1 = require("../example/ShipGen");
+Object.defineProperty(exports, "ShipGenerator", { enumerable: true, get: function () { return ShipGen_1.ShipGenerator; } });
--- src/lib/main.ts
+++ src/lib/main.ts
@@ -4,3 +4,9 @@ export { Client } from "./Client/Client";
 export { Setup } from "./Client/Setup";
 export { AsteroidBuilder } from "./GameTypes/GameMechanics/Asteroid";
 export { connect, disconnect } from "./GameApi/Database/Database";
+
+export { AttachmentGenerator } from "../example/AttachmentGen";
+export { FactionGenerator } from "../example/FactionGen";
+export { MaterialGenerator } from "../example/MaterialGen";
+export { NodeGenerator } from "../example/NodeGen";
+export { ShipGenerator } from "../example/ShipGen";

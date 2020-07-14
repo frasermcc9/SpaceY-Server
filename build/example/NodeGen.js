ae8d1855ea67128298b5f6821a881045df8f6f69
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-14 17:31:33 +1200

    Added more nodes

--- build/example/NodeGen.js
+++ build/example/NodeGen.js
@@ -7,12 +7,17 @@ const Asteroid_1 = require("../lib/GameTypes/GameMechanics/Asteroid");
 const MaterialStore_1 = require("../lib/GameTypes/GameStore/MaterialStore");
 const BaseStore_1 = require("../lib/GameTypes/GameStore/BaseStore");
 const ShipStore_1 = require("../lib/GameTypes/GameStore/ShipStore");
+const AttachmentStore_1 = require("../lib/GameTypes/GameStore/AttachmentStore");
 exports.NodeGenerator = () => {
+    return [...KalenGen()];
+};
+const KalenGen = () => {
     return [
         new SpacemapNode_1.SpacemapNodeBuilder({
             name: "Gemini",
             requiredWarp: SpacemapNode_1.WarpPower.NONE,
             faction: main_1.Client.Reg.ResolveFactionFromName("Kalen"),
+            img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
         })
             .addAsteroid(new Asteroid_1.AsteroidBuilder("GEM01").BuildRandom({ value: 500 }))
             .addAsteroid(new Asteroid_1.AsteroidBuilder("GEM02").BuildRandom({ value: 500 }))
@@ -25,16 +30,17 @@ exports.NodeGenerator = () => {
             marketForces: true,
             centralRarity: 1,
             minRarity: 0,
-            maxRarity: 6,
+            maxRarity: 4,
             enableRarityEffects: true,
             minTech: 0,
-            maxTech: 4,
+            maxTech: 3,
         }))
             .build(),
         new SpacemapNode_1.SpacemapNodeBuilder({
             name: "Kalen",
             requiredWarp: SpacemapNode_1.WarpPower.NONE,
             faction: main_1.Client.Reg.ResolveFactionFromName("Kalen"),
+            img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
         })
             .addAsteroid(new Asteroid_1.AsteroidBuilder("KAL01").BuildRandom({ value: 750 }))
             .addStore(new MaterialStore_1.MaterialStore({
@@ -45,16 +51,17 @@ exports.NodeGenerator = () => {
             marketForces: true,
             centralRarity: 1,
             minRarity: 0,
-            maxRarity: 7,
+            maxRarity: 4,
             enableRarityEffects: true,
             minTech: 0,
-            maxTech: 5,
+            maxTech: 3,
         }))
             .build(),
         new SpacemapNode_1.SpacemapNodeBuilder({
             name: "Lyra",
             requiredWarp: SpacemapNode_1.WarpPower.NONE,
             faction: main_1.Client.Reg.ResolveFactionFromName("Kalen"),
+            img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
         })
             .addStore(new ShipStore_1.ShipStore({
             initialCredits: 25000,
@@ -62,6 +69,55 @@ exports.NodeGenerator = () => {
             type: BaseStore_1.StoreType.SHIP_STORE,
             maxToSell: 2,
             storeFaction: main_1.Client.Reg.ResolveFactionFromName("Kalen"),
+            marketForces: true,
+        }))
+            .build(),
+        new SpacemapNode_1.SpacemapNodeBuilder({
+            name: "Aries",
+            requiredWarp: SpacemapNode_1.WarpPower.NONE,
+            faction: main_1.Client.Reg.ResolveFactionFromName("Kalen"),
+            img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
+        })
+            .addAsteroid(new Asteroid_1.AsteroidBuilder("ARI01").BuildRandom({ value: 450 }))
+            .addAsteroid(new Asteroid_1.AsteroidBuilder("ARI02").setCooldown(60 * 30).BuildRandom({ value: 1250 }))
+            .addStore(new MaterialStore_1.MaterialStore({
+            initialCredits: 1900,
+            storeName: "Aries Emporium",
+            type: BaseStore_1.StoreType.MATERIAL_STORE,
+            generationValue: 1700,
+            marketForces: true,
+            centralRarity: 3,
+            minRarity: 0,
+            maxRarity: 5,
+            enableRarityEffects: true,
+            minTech: 1,
+            maxTech: 4,
+        }))
+            .build(),
+        new SpacemapNode_1.SpacemapNodeBuilder({
+            name: "Auriga",
+            requiredWarp: SpacemapNode_1.WarpPower.NONE,
+            faction: main_1.Client.Reg.ResolveFactionFromName("Kalen"),
+            img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
+        })
+            .addAsteroid(new Asteroid_1.AsteroidBuilder("AGA01").BuildRandom({ value: 500 }))
+            .addAsteroid(new Asteroid_1.AsteroidBuilder("AGA02").BuildRandom({ value: 500 }))
+            .addAsteroid(new Asteroid_1.AsteroidBuilder("AGA03").BuildRandom({ value: 500 }))
+            .addAsteroid(new Asteroid_1.AsteroidBuilder("AGA04").BuildRandom({ value: 500 }))
+            .build(),
+        new SpacemapNode_1.SpacemapNodeBuilder({
+            name: "Orion",
+            requiredWarp: SpacemapNode_1.WarpPower.NONE,
+            faction: main_1.Client.Reg.ResolveFactionFromName("Kalen"),
+            img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
+        })
+            .addStore(new AttachmentStore_1.AttachmentStore({
+            initialCredits: 2650,
+            storeName: "Orion Ordinance",
+            maxToSell: 1,
+            storeFaction: main_1.Client.Reg.ResolveFactionFromName("Kalen"),
+            type: BaseStore_1.StoreType.SHIP_STORE,
+            marketForces: true,
         }))
             .build(),
     ];
--- src/example/NodeGen.ts
+++ src/example/NodeGen.ts
@@ -4,13 +4,19 @@ import { AsteroidBuilder } from "../lib/GameTypes/GameMechanics/Asteroid";
 import { MaterialStore } from "../lib/GameTypes/GameStore/MaterialStore";
 import { StoreType } from "../lib/GameTypes/GameStore/BaseStore";
 import { ShipStore } from "../lib/GameTypes/GameStore/ShipStore";
+import { AttachmentStore } from "../lib/GameTypes/GameStore/AttachmentStore";
 
 export const NodeGenerator = () => {
+	return [...KalenGen()];
+};
+
+const KalenGen = () => {
 	return [
 		new SpacemapNodeBuilder({
 			name: "Gemini",
 			requiredWarp: WarpPower.NONE,
 			faction: Client.Reg.ResolveFactionFromName("Kalen")!,
+			img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
 		})
 			.addAsteroid(new AsteroidBuilder("GEM01").BuildRandom({ value: 500 }))
 			.addAsteroid(new AsteroidBuilder("GEM02").BuildRandom({ value: 500 }))
@@ -24,10 +30,10 @@ export const NodeGenerator = () => {
 					marketForces: true,
 					centralRarity: 1,
 					minRarity: 0,
-					maxRarity: 6,
+					maxRarity: 4,
 					enableRarityEffects: true,
 					minTech: 0,
-					maxTech: 4,
+					maxTech: 3,
 				})
 			)
 			.build(),
@@ -35,6 +41,7 @@ export const NodeGenerator = () => {
 			name: "Kalen",
 			requiredWarp: WarpPower.NONE,
 			faction: Client.Reg.ResolveFactionFromName("Kalen")!,
+			img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
 		})
 			.addAsteroid(new AsteroidBuilder("KAL01").BuildRandom({ value: 750 }))
 			.addStore(
@@ -46,10 +53,10 @@ export const NodeGenerator = () => {
 					marketForces: true,
 					centralRarity: 1,
 					minRarity: 0,
-					maxRarity: 7,
+					maxRarity: 4,
 					enableRarityEffects: true,
 					minTech: 0,
-					maxTech: 5,
+					maxTech: 3,
 				})
 			)
 			.build(),
@@ -57,6 +64,7 @@ export const NodeGenerator = () => {
 			name: "Lyra",
 			requiredWarp: WarpPower.NONE,
 			faction: Client.Reg.ResolveFactionFromName("Kalen")!,
+			img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
 		})
 			.addStore(
 				new ShipStore({
@@ -65,6 +73,62 @@ export const NodeGenerator = () => {
 					type: StoreType.SHIP_STORE,
 					maxToSell: 2,
 					storeFaction: Client.Reg.ResolveFactionFromName("Kalen")!,
+					marketForces: true,
+				})
+			)
+			.build(),
+		new SpacemapNodeBuilder({
+			name: "Aries",
+			requiredWarp: WarpPower.NONE,
+			faction: Client.Reg.ResolveFactionFromName("Kalen")!,
+			img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
+		})
+			.addAsteroid(new AsteroidBuilder("ARI01").BuildRandom({ value: 450 }))
+			.addAsteroid(new AsteroidBuilder("ARI02").setCooldown(60 * 30).BuildRandom({ value: 1250 }))
+			.addStore(
+				new MaterialStore({
+					initialCredits: 1900,
+					storeName: "Aries Emporium",
+					type: StoreType.MATERIAL_STORE,
+					generationValue: 1700,
+					marketForces: true,
+					centralRarity: 3,
+					minRarity: 0,
+					maxRarity: 5,
+					enableRarityEffects: true,
+					minTech: 1,
+					maxTech: 4,
+				})
+			)
+
+			.build(),
+
+		new SpacemapNodeBuilder({
+			name: "Auriga",
+			requiredWarp: WarpPower.NONE,
+			faction: Client.Reg.ResolveFactionFromName("Kalen")!,
+			img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
+		})
+			.addAsteroid(new AsteroidBuilder("AGA01").BuildRandom({ value: 500 }))
+			.addAsteroid(new AsteroidBuilder("AGA02").BuildRandom({ value: 500 }))
+			.addAsteroid(new AsteroidBuilder("AGA03").BuildRandom({ value: 500 }))
+			.addAsteroid(new AsteroidBuilder("AGA04").BuildRandom({ value: 500 }))
+			.build(),
+
+		new SpacemapNodeBuilder({
+			name: "Orion",
+			requiredWarp: WarpPower.NONE,
+			faction: Client.Reg.ResolveFactionFromName("Kalen")!,
+			img: "https://cdn.discordapp.com/attachments/732459193545523261/732462067436617778/unknown.png",
+		})
+			.addStore(
+				new AttachmentStore({
+					initialCredits: 2650,
+					storeName: "Orion Ordinance",
+					maxToSell: 1,
+					storeFaction: Client.Reg.ResolveFactionFromName("Kalen")!,
+					type: StoreType.SHIP_STORE,
+					marketForces: true,
 				})
 			)
 			.build(),

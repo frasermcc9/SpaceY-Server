a915b92fad6d8c248d4d8b73a3be063accde070d
Fraser McCallum <fraser@themccallums.net.nz>
2020-08-18 18:28:29 +1200

    Added optimisations to avoid validation when the input is definitely correct

--- src/lib/GameTypes/GameCollections/PlayerMaterialCollection.ts
+++ src/lib/GameTypes/GameCollections/PlayerMaterialCollection.ts
@@ -1,7 +1,13 @@
 import { MaterialCollection, IMaterialCollectionOptions } from "./MaterialCollection";
 import { Player } from "../GameAsset/Player/Player";
 import { Client } from "../../main";
+import { Server } from "../../Server/Server";
 
+/** Collection of materials for a player. Adds more restrictions, due to cargo
+ * space. Set override ensures that materials are not added if cargo is full.
+ * Players can exceed cargo limit if they reduce their cargo size below their
+ * current amount. In this case, the excess cargo is not lost, but no more can
+ * be added (just as if it were full). */
 export class PlayerMaterialCollection extends MaterialCollection {
     private owner?: Player;
 
@@ -18,12 +24,31 @@ export class PlayerMaterialCollection extends MaterialCollection {
         if (diff >= 0) {
             return super.set(key, value);
         } else {
-            return super.set(key, value + diff);
+            return super.set(key, Math.max(value + diff, current ?? 0));
         }
     }
 
+    /**
+     * same as normal set, but does not check for cargo changes. Used for
+     * construction from database since these values do not need to be checked
+     * for overcapacity.
+     * @param key
+     * @param value
+     */
+    private quickSet(key: string, value: number): this {
+        return super.set(key, value);
+    }
+
     public constructor(options?: IMaterialCollectionOptions) {
-        super(options);
+        super();
+        if (options == undefined) return;
+        let data: any;
+        if (options.data instanceof Map) data = Object.fromEntries(options.data);
+        else data = options.data;
+
+        Server.Get().Registry.MaterialRegistry.forEach((material) => {
+            this.quickSet(material.Name, data[material.Name] ?? 0);
+        });
     }
 
     public set Owner(player: Player) {
